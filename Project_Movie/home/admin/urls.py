8181b9fbd2c09be4f5fa75b032a0c3737abe2f0d
sunwei <1044507840@qq.com>
2020-11-17 14:22:19 +0800

    注释

--- Project_Movie/home/admin/urls.py
+++ Project_Movie/home/admin/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
-
+# 是是是
 ]
